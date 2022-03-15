823751b70a1342767640209183e73a6769877573
CallumSmith789 <smeeto96@gmail.com>
2022-03-15 16:05:01 +0000

    first commit

---
+++ src/HelloWorld/helloworld.java
@@ -0,0 +1,23 @@
+package HelloWorld;
+
+public class helloworld {
+	
+	//public static void printMessage() {
+		//String message = "Hello World!";
+		//System.out.println(message);
+	//}
+	//
+	//public static void main(String[] args) {
+		//printMessage();
+	//}
+//}
+
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+	
+	public static void main(String[] args) {
+		printMessage("Hello World!");
+	}
+		
+	}
